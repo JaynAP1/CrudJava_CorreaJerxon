df8ef32a97067a345f41653d1ad3718244a68c53
JaynAP1 <jerxonmjair@gmail.com>
2025-02-13 12:56:04 -0500

    :sparkles: Pruebas de interfaz para proyecto

---
+++ PruebasInterfaz/src/pruebasinterfaz/PruebasInterfaz.java
@@ -0,0 +1,16 @@
+
+package pruebasinterfaz;
+
+import igu.Pantalla;
+
+public class PruebasInterfaz {
+
+  
+    public static void main(String[] args) {
+        
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocation(0, 0);
+    }
+    
+}
